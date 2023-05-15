feb62628c57c0085087db9f427659ed81d8a2efd
Chris_707 <eliasgonzalesec@gmail.com>
2023-05-15 15:08:35 +0000

    ShowDialogs Update

---
+++ Heracles/MyShowDialog/winInsertDialog.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Heracles.MyShowDialog
+{
+    public partial class winInsertDialog : Window
+    {
+        public winInsertDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccept_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Heracles/MyShowDialog/winUpdateDialog.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Heracles.MyShowDialog
+{
+    public partial class winUpdateDialog : Window
+    {
+        public winUpdateDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccept_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();   
+        }
+    }
+}
